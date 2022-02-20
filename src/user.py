05bdcfc1dafd07086d1126b7f9a891b2b5735b20
andrewfritz2001 <andrew_fritz@outlook.com>
2022-02-19 21:22:43 -0800

    added skeleton code for simple required course func calls

--- src/user.py
+++ src/user.py
@@ -1,7 +1,7 @@
-# Will act as current user. Right now program only works with one user at a time, although this can be scaled later
-
 from course_taken import CourseTaken
+from cpts_query_tool import CptsQueryTool
 
+# Will act as current user. Right now program only works with one user at a time, although this can be scaled later
 class User:
     def __init__(self):
         self.courses_taken = [] # ["CPTS_121", "CPTS_122", "CPTS_360", "PHIL_201"]
@@ -9,9 +9,12 @@ class User:
         self.id_num = 0         # 069696969
         self.major = ""         # Marine Bobology
 
-    def get_required_courses():
+    def get_required_courses(self):
         # function to call a db class with self.major and self.courses_taken. function returns list of classes that are still required. 
-        pass
+        query = CptsQueryTool(self.major, self.courses_taken)
+        print("Required Data Information Management")
+        print(query.get_dim_reqs())
+
 
     # 0 = course couldn't be added (not found or already taken), 1 = course added
     def add_course(self, course_name : str) -> bool:

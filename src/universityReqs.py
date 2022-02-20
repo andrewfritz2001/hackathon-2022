6098c21c136f034879f0ceb47cab9e67207827ea
andrewfritz2001 <andrew_fritz@outlook.com>
2022-02-19 16:52:00 -0800

    added dictionary mapping UCORE reqs to courses

---
+++ src/universityReqs.py
@@ -0,0 +1,33 @@
+
+# A dictonary for defining university requirements
+# Only first four groups are comprehensive
+university_reqs = {
+    "UCORES" : {"ROOT" : {"HIST_105", "HIST_305"},
+                "COMM" : {"COMM_102", "COMM_210", "COMM_400", "ENG_106", "FRENCH_361", "GERMAN_307", "GERMAN_361"},
+                "QUAN" : {"CPTS_111", "ECON_335", "ENGR_107", "FIN_223", "MATH_105", "MATH__140", "MATH_171", "MATH_202", "MATH_252", "PHIL_201", "PSYCH_311", "STAT_212"},
+                "WRTG" : {"ENGLISH_101", "ENGLISH_105", "ENGLISH_201", "ENGLISH_301", "ENGLISH_365", "ENGLISH_402", "ENGLISH_403", "PHIL_200"},
+
+                "ART"  : {"AMDT_408", "ANTH_301", "DTC_101", "DTC_201", "ENGLISH_150", "ENGLISH_212", "FINE_ART_101", "FINE_ART_102", "FINE_ART_103", "FINE_ART_110", "FINE_ART_201", "MUS_120", 
+                            "MUS_153", "MUS_160", "MUS_163", "MUS_262", "MUS_266", "SDC_100", "SPANISH_110", "SPANISH_111"}, 
+                "BSCI" : {"ANIM_SCI_205", "ANTH_260", "ANTH_268", "ANTH_280", "ANTH_381", "BIOLOGY_101", "BIOLOGY _102", "BIOLOGY_106", "BIOLOGY_107", "BIOLOGY_111", "BIOLOGY_120", 
+                           "BIOLOGY_140", "BIOLOGY_150", "BIOLOGY_298", "BIOLOGY_308", "BIOLOGY_333", "ENTOM_101", "ENTOM_103", "ENTOM_150", "ENTOM_201", "FS_201", "HORT_150", "MBIOS_101", 
+                           "MBIOS_320", "NEUROSCI_105", "NEUROSCI_150", "PSYCH_372", "SCIENCE_102", "SOE_110", "SOIL_SCI_201", "SOIL_SCI_202"},
+                "HUM"  : {"ANTH_201", "ANTH_355", "CAS_310", "CES_111", "CES_151", "CES_209", "ENGLISH_210", "ENGLISH_219", "HISTORY_447", "HISTORY_454", "HUMANITY_101", "HUMANITY_103", 
+                            "HUMANITY_302", "HUMANITY_304", "JAPANESE_123", "KINES_201", "LND_ARCH_150", "MUS_271", "CES_271", "MUS_359", "MUS_360", "PHIL_101", "PHIL_103", "PHIL_207", "PHIL_210", 
+                            "PHIL_220", "PHIL_280", "PHIL_314", "PHIL_315", "PHIL_360", "PHIL_365", "PHIL_370", "PHIL_450", "SPANISH_120", "SPANISH_121", "WGSS_211", "WGSS_338"}, 
+                "PSCI" : {"AMDT_210", "ASTRONOM_135", "ASTRONOM_138", "ASTRONOM_390", "CHEM_101", "CHEM_105", "PHYSICS_101", "PHYSICS_111", "PHYSICS_137", "PHYSICS_150", "PHYSICS_01	", 
+                            "PHYSICS_205", "PHYSICS_211", "PHYSICS_322", "SCIENCE_101", "SOE _101", "SOE_103", "SOE_105", "SOE_210", "SOE_230", "SOE_250", "SOE_280"}, 
+                "SSCI" : {"AFS_336", "ANTH_130", "ANTH_135", "ANTH_205", "ANTH_232", "ANTH_302", "ANTH_304", "ANTH_305", "ANTH_309", "COM_101", "CRM_J_101", "ECONS_101", "ECONS_102", "H_D_101", 
+                            "NEP_200", "POL_S_101", "POL_S_102", "POL_S_103", "PSYCH_105", "SOC_101", "SOC_10"},
+                "DIVR" : {"ANTH_101", "ANTH_203", "ANTH_220", "ANTH_307", "BIOLOGY_307", "CES_101", "CES_291", "CES_325", "COMSOC_321", "COUN_PSY_457", "CRM_J_205", "DTC_206", "ENGLISH_362", 
+                            "ENGLISH_489", "FOR_LANG_110", "H_D_350", "HISTORY_120", "HISTORY_130", "HISTORY_150", "HISTORY_321", "HISTORY_322", "SOE_312", "SPANISH_321", "SPMGT_101", "WGSS_101", 
+                            "WGSS_120", "WGSS_22"}, 
+                "CAPS" : {"ANIM_SCI_464", "ANIM_SCI_472", "ANIM_SCI_474", "ANTH_404", "ANTH_464", "ANTH_490", "ARCH_403", "ASTRONOM_450", "BIO_ENG_411", "BIOLOGY_401", "BIOLOGY_408", "BIOLOGY_485", 
+                            "BIOLOGY_489", "CAS_410", "CES_489", "CHE_451", "CHEM_485", "COM_471", "COMSOC_421", "CPT_S_423", "CROP_SCI_435", "CST_M_475", "DATA_424", "DTC_497", "E_E_416", 
+                            "ECE_452", "ECONS_490", "ENGLISH_415", "ENGLISH_446", "ENGLISH_494", "ENGR_421", "ENGR_431", "ENTRP_492", "FINE_ART_408", "FINE_ART_498", "FOR_LANG_410", "FRENCH_410", 
+                            "FRENCH_420", "FRENCH_430", "FS_489", "GERMAN	_420", "HBM_475", "HBM_493", "HBM_495", "H_D_403", "H_D_415", "H_D_418", "HISTORY_409", "HISTORY_417", "HISTORY_435", 
+                            "HISTORY_436", "HISTOR_444"} },          
+    "Credits" : 120,
+    "Upper-Division Credits" : 40,
+    "GPA" : 2.0
+}

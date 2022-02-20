3370df08e47d32100d92070e9beccb9d011f7dd7
andrewfritz2001 <andrew_fritz@outlook.com>
2022-02-19 17:11:46 -0800

    CourseTaken class

---
+++ src/CourseTaken.py
@@ -0,0 +1,10 @@
+# For storing user information about which courses they have taken.
+
+class CourseTaken:
+    def __init(self):
+        self.course_name = ""   # CPTS_121
+        self.course_title = ""  # Program Design and Analysis
+        self.grade = ""         # A-
+        self.credits = 0        # 3
+        self.term = ""          # Spring 2022
+        self.type = ""          # EN || IP || TE  ==> Enrolled || In Progress || Transfer
